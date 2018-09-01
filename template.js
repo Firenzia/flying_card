27ea9f2a2fd93dee3dc89c6d3b4e5b081e75422a
xiesi <249428629@qq.com>
2018-09-01 23:17:29 +0800

    update src

---
+++ template.js
@@ -0,0 +1,13 @@
+var fragement = ['<div class="photo photo-front" id="card_{{id}}" onclick="turn(this)">',
+				'<div class="photo-wrap">',
+				'<div class="side front-side">',
+				'<img src="img/{{img}}_1.GIF" alt="">',
+				'</div>',
+				'<div class="side back-side"> ',
+				'<img src="img/{{img}}_2.GIF" alt="">',
+				'</div> ',
+				'</div> ',
+				'</div> '
+]
+var card_template= fragement.join('')
+
